cba690edab566ce994de5c4006a6dc5f938f0d9a
nachi0310 <nachiketwarhade@gmail.com>
2020-11-28 11:50:56 +0530

    sitemap file variable updation

--- sitemap.js
+++ sitemap.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const format = require('xml-formatter');
 const { Readable } = require( 'stream' )
-var link = process.argv[2];
+const link = process.argv[2];
 if(!link) {
   console.log("please provide a valid link")
 }
@@ -21,55 +21,54 @@ if(link) {
         page.evaluate(() => history.pushState(null, null, null)),
     ]);
     //await page.waitForSelector('.tournament-name');
-    var result = await page.evaluateHandle(() => {
-      var hrefLink = []
+    const result = await page.evaluateHandle(() => {
+      const hrefLink = []
       document.querySelectorAll('a').forEach(selector => {
         if(selector.hasAttribute("href")) {
           hrefLink.push(selector)
         }
       })
-      var urlString = "";
+      let urlString = "";
       hrefLink.forEach(href => {  
         urlString = urlString + " " + href.getAttribute("href");
       })
       return urlString;
     })
-    var modifiedResult = "";
-      result.toString().split(" ").forEach(resultLink => {
-        if(resultLink !== "undefined") {  
-          modifiedResult = modifiedResult + " " + resultLink;
-        }  
-        })
-        var resultList = modifiedResult.toString().split(" ");
-        resultList = resultList.slice(2);
-        var updatedList = [];
-        resultList.forEach(result => {
-          if((result.indexOf('http') > -1 || result.indexOf("https") > -1)&& result !== "/") {
-            updatedList.push(result);
-          } else if(result !== '/') {
-            updatedList.push(link + result);
-          } 
-        })
-        console.log(updatedList);
-        const streamLinks = [];
-        updatedList.forEach(result => {
-          var newStream = {
-            url: result,
-            changeFreq: 'daily',
-            priority: 0.8
-          }
-          streamLinks.push(newStream)
-        })
-        const stream = new SitemapStream({ hostname: link })
-        streamToPromise(Readable.from(streamLinks).pipe(stream)).then((data) => {
-          fs.appendFile('sitemap.xml', format(data.toString()), (err) => {
-            if (err) throw err;
-            // file saved
-            console.log('xml saved!');
-          });
-        })
-      })();
-    } catch (e) {
-      console.error(e);
-    }
+    let modifiedResult = "";
+    result.toString().split(" ").forEach(resultLink => {
+      if(resultLink !== "undefined") {  
+        modifiedResult = modifiedResult + " " + resultLink;
+      }  
+    })
+    let resultList = modifiedResult.toString().split(" ");
+    resultList = resultList.slice(2);
+    const updatedList = [];
+    resultList.forEach(result => {
+      if((result.indexOf('http') > -1 || result.indexOf("https") > -1)&& result !== "/") {
+        updatedList.push(result);
+      } else if(result !== '/') {
+        updatedList.push(link + result);
+      } 
+    })
+    const streamLinks = [];
+      updatedList.forEach(result => {
+      const newStream = {
+        url: result,
+        changeFreq: 'daily',
+        priority: 0.8
+      }
+      streamLinks.push(newStream)
+    })
+    const stream = new SitemapStream({ hostname: link })
+    streamToPromise(Readable.from(streamLinks).pipe(stream)).then((data) => {
+      fs.appendFile('sitemap.xml', format(data.toString()), (err) => {
+        if (err) throw err;
+          // file saved
+          console.log('xml saved!');
+        });
+      })
+    })();
+  } catch (e) {
+    console.error(e);
+  }
 } 
